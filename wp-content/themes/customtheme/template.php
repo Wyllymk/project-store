36cd77b2d7cb694f2c6b6355cd36a5d4a39ec75c
Christine Bai <bai20726@gmail.com>
2023-02-21 16:05:06 +0300

    changes to the function.php for user to remove item from wishlist and they can add to cart

--- wp-content/themes/customtheme/template.php
+++
@@ -1,5 +0,0 @@
-<form method="post" action="<?php echo admin_url('admin-post.php'); ?>">
-    <input type="hidden" name="action" value="add_to_wishlist">
-    <input type="hidden" name="post_id" value="<?php echo get_the_ID(); ?>">
-    <button type="submit" class="wishlist-button">Add to Wishlist</button>
-</form>
