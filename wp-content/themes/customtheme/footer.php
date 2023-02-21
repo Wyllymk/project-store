0d735f693402f1b95c92ba79e88ff116cc8a4023
Christine Bai <bai20726@gmail.com>
2023-02-21 16:51:17 +0300

    remove the wishlist error

---
+++ wp-content/themes/customtheme/footer.php
@@ -0,0 +1,24 @@
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Get connected with us on social networks:</span>
+    </div>
+    <!-- Left -->
+
+    
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2023 Copyright:
+    <a class="text-reset fw-bold" href="">Cohort 8</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+
+        <?php wp_footer();?>
+    </body>
+</html>
--- wp-content/themes/customtheme/page-wishlist.php
+++ wp-content/themes/customtheme/page-wishlist.php
@@ -1,5 +1,27 @@
-<?php ?>
-<?php get_header(); ?>
+
+<?php
+
+get_header();
+ 
+function display_wishlist() {
+    if (isset($_SESSION['wishlist'])) {
+        $wishlist = $_SESSION['wishlist'];
+        if (!empty($wishlist)) {
+            echo '<ul>';
+            foreach($wishlist as $product_id) {
+                $product = wc_get_product($product_id);
+                echo '<li>' . $product->get_name() . ' - <a href="?remove_from_wishlist=' . $product_id . '">Remove</a> - <a href="?add_to_cart=' . $product_id . '">Add to cart</a></li>';
+            }
+            echo '</ul>';
+        } else {
+            echo '<p>Your wishlist is empty</p>';
+        }
+    } else {
+        echo '<p>Your wishlist is empty</p>';
+    }
+}
+
+?>
 
 <div class="container">
   <h1>Wishlist</h1>
@@ -7,3 +29,4 @@
 </div>
 
 <?php get_footer(); ?>
+
